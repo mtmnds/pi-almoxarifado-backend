f20cec207ccd7c7b6daa3ac258deba942a766780
Bruna Taura <bruna.taura@hotmail.com>
2020-10-17 18:55:29 -0300

    adiciona crud de material

--- src/main/java/br/fatec/app/modules/v1/material/MaterialService.java
+++ src/main/java/br/fatec/app/modules/v1/material/MaterialService.java
@@ -1,8 +1,12 @@
 package br.fatec.app.modules.v1.material;
 
 
+import br.fatec.app.modules.v1.material.entity.MaterialEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -17,4 +21,38 @@ public class MaterialService {
     ) {
         this.materialRepository = materialRepository;
     }
+
+
+    public MaterialEntity cadastrarMaterial(MaterialEntity material) {
+        return this.materialRepository.save(material);
+    }
+
+
+    public MaterialEntity alterarMaterial(MaterialEntity material) {
+        return this.materialRepository.save(material);
+    }
+
+
+    public MaterialEntity buscarMaterial(long idMaterial) {
+        Optional<MaterialEntity> material = this.materialRepository.findById(idMaterial);
+        return material.orElse(null);
+    }
+
+
+    public List<MaterialEntity> listarTodosMateriais() {
+        return this.materialRepository.findAll();
+    }
+
+
+    public void desativarMaterial(long idMaterial) {
+        Optional<MaterialEntity> materialEntity = this.materialRepository.findById(idMaterial);
+
+        if (materialEntity.isPresent()) {
+            MaterialEntity material = materialEntity.get();
+            material.setAtivo(false);
+            this.materialRepository.save(material);
+        }
+    }
+
+
 }
