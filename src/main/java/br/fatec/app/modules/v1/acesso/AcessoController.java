909ac3b1e1ba2288bbcc032a5c3167b5d41fdbe9
Bruna Taura <bruna.taura@hotmail.com>
2020-10-17 22:38:30 -0300

    adiciona crud acesso

--- src/main/java/br/fatec/app/modules/v1/acesso/AcessoController.java
+++ src/main/java/br/fatec/app/modules/v1/acesso/AcessoController.java
@@ -1,9 +1,15 @@
 package br.fatec.app.modules.v1.acesso;
 
+import br.fatec.app.modules.v1.acesso.entity.AcessoEntity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -20,4 +26,90 @@ public class AcessoController {
         this.acessoService = acessoService;
     }
 
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = {"", "/"},
+            consumes = "application/json",
+            produces = "application/json"
+    )
+    public ResponseEntity<AcessoEntity> cadastrarAcesso(@RequestBody AcessoEntity acesso) {
+        try {
+            AcessoEntity resposta = this.acessoService.cadastrarAcesso(acesso);
+            return ResponseEntity.ok(resposta);
+
+        }
+        catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            value = {"", "/"},
+            consumes = "application/json",
+            produces = "application/json"
+    )
+    public ResponseEntity<AcessoEntity> alterarAcesso(@RequestBody AcessoEntity acesso) {
+        try {
+            AcessoEntity resposta = this.acessoService.alterarAcesso(acesso);
+            return ResponseEntity.ok(resposta);
+
+        }
+        catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/{idAcesso}",
+            produces = "application/json"
+    )
+    public ResponseEntity<AcessoEntity> buscarAcesso(@PathVariable("idAcesso") long idAcesso) {
+        try {
+            AcessoEntity resposta = this.acessoService.buscarAcesso(idAcesso);
+            return ResponseEntity.ok(resposta);
+
+        }
+        catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = {"", "/"},
+            produces = "application/json"
+    )
+    public ResponseEntity<List<AcessoEntity>> listarTodosAcessos() {
+        try {
+            List<AcessoEntity> resposta = this.acessoService.listarTodosAcessos();
+            return ResponseEntity.ok(resposta);
+
+        }
+        catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            value = "/{idAcesso}",
+            produces = "application/json"
+    )
+    public ResponseEntity<AcessoEntity> desativarAcesso(@PathVariable("idAcesso") long idAcesso) {
+        try {
+            this.acessoService.desativarAcesso(idAcesso);
+            return ResponseEntity.ok().build();
+
+        }
+        catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
--- src/main/java/br/fatec/app/modules/v1/acesso/AcessoService.java
+++ src/main/java/br/fatec/app/modules/v1/acesso/AcessoService.java
@@ -1,8 +1,12 @@
 package br.fatec.app.modules.v1.acesso;
 
-
+import br.fatec.app.modules.v1.acesso.entity.AcessoEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -16,6 +20,38 @@ public class AcessoService {
             AcessoRepository acessoRepository
     ) {
         this.acessoRepository = acessoRepository;
+    }
+
+
+    public AcessoEntity cadastrarAcesso(AcessoEntity acesso) {
+        return this.acessoRepository.save(acesso);
+    }
+
+
+    public AcessoEntity alterarAcesso(AcessoEntity acesso) {
+        return this.acessoRepository.save(acesso);
+    }
+
+
+    public AcessoEntity buscarAcesso(long idAcesso) {
+        Optional<AcessoEntity> acesso = this.acessoRepository.findById(idAcesso);
+        return acesso.orElse(null);
+    }
+
+
+    public List<AcessoEntity> listarTodosAcessos() {
+        return this.acessoRepository.findAll();
+    }
+
+
+    public void desativarAcesso(long idAcesso) {
+        Optional<AcessoEntity> acessoEntity = this.acessoRepository.findById(idAcesso);
+
+        if (acessoEntity.isPresent()) {
+            AcessoEntity acesso = acessoEntity.get();
+            acesso.setAtivo(false);
+            this.acessoRepository.save(acesso);
+        }
     }
 
 }
