68b7ee3ec71d75433293a53e7e3ca4ad1132371e
Bruna Taura <bruna.taura@hotmail.com>
2020-10-17 21:48:54 -0300

    adiciona crud local estoque

--- src/main/java/br/fatec/app/modules/v1/localestoque/LocalEstoqueService.java
+++ src/main/java/br/fatec/app/modules/v1/localestoque/LocalEstoqueService.java
@@ -1,8 +1,12 @@
 package br.fatec.app.modules.v1.localestoque;
 
-
+import br.fatec.app.modules.v1.localestoque.entity.LocalEstoqueEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -16,6 +20,38 @@ public class LocalEstoqueService {
             LocalEstoqueRepository localEstoqueRepository
     ) {
         this.localEstoqueRepository = localEstoqueRepository;
+    }
+
+
+    public LocalEstoqueEntity cadastrarLocalEstoque(LocalEstoqueEntity localEstoque) {
+        return this.localEstoqueRepository.save(localEstoque);
+    }
+
+
+    public LocalEstoqueEntity alterarLocalEstoque(LocalEstoqueEntity localEstoque) {
+        return this.localEstoqueRepository.save(localEstoque);
+    }
+
+
+    public LocalEstoqueEntity buscarLocalEstoque(long idLocalEstoque) {
+        Optional<LocalEstoqueEntity> localEstoque = this.localEstoqueRepository.findById(idLocalEstoque);
+        return localEstoque.orElse(null);
+    }
+
+
+    public List<LocalEstoqueEntity> listarTodosLocaisEstoque() {
+        return this.localEstoqueRepository.findAll();
+    }
+
+
+    public void desativarLocalEstoque(long idLocalEstoque) {
+        Optional<LocalEstoqueEntity> localEstoqueEntity = this.localEstoqueRepository.findById(idLocalEstoque);
+
+        if (localEstoqueEntity.isPresent()) {
+            LocalEstoqueEntity localEstoque = localEstoqueEntity.get();
+            localEstoque.setAtivo(false);
+            this.localEstoqueRepository.save(localEstoque);
+        }
     }
 
 }
