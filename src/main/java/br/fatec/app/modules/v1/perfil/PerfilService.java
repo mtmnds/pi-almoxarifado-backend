aa8ea820fb4c8897416f9f9e1867d9a78fd25972
Bruna Taura <bruna.taura@hotmail.com>
2020-10-17 22:25:36 -0300

    adiciona crud perfil

--- src/main/java/br/fatec/app/modules/v1/perfil/PerfilService.java
+++ src/main/java/br/fatec/app/modules/v1/perfil/PerfilService.java
@@ -1,8 +1,12 @@
 package br.fatec.app.modules.v1.perfil;
 
-
+import br.fatec.app.modules.v1.perfil.entity.PerfilEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -16,6 +20,38 @@ public class PerfilService {
             PerfilRepository perfilRepository
     ) {
         this.perfilRepository = perfilRepository;
+    }
+
+
+    public PerfilEntity cadastrarPerfil(PerfilEntity perfil) {
+        return this.perfilRepository.save(perfil);
+    }
+
+
+    public PerfilEntity alterarPerfil(PerfilEntity perfil) {
+        return this.perfilRepository.save(perfil);
+    }
+
+
+    public PerfilEntity buscarPerfil(long idPerfil) {
+        Optional<PerfilEntity> perfil = this.perfilRepository.findById(idPerfil);
+        return perfil.orElse(null);
+    }
+
+
+    public List<PerfilEntity> listarTodosPerfis() {
+        return this.perfilRepository.findAll();
+    }
+
+
+    public void desativarPerfil(long idPerfil) {
+        Optional<PerfilEntity> perfilEntity = this.perfilRepository.findById(idPerfil);
+
+        if (perfilEntity.isPresent()) {
+            PerfilEntity perfil = perfilEntity.get();
+            perfil.setAtivo(false);
+            this.perfilRepository.save(perfil);
+        }
     }
 
 }
